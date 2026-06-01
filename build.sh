c116f1d735fcf44c8d62019b2bc5820365461c16
Vlad Gerasimov <vlad@vlad.studio>
2026-06-01 15:44:16 +0400

    Rename scripts/ to app-scripts/

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$0")"
-source ../scripts/build-kit.sh
+source ../app-scripts/build-kit.sh
 build_app "Stat" \
   --info app/Stat/Info.plist \
   --resources "icons/cpu.png icons/gpu.png icons/temp.png icons/download-k.png icons/download-m.png icons/upload-k.png icons/upload-m.png icons/day-sun.png icons/day-mon.png icons/day-tue.png icons/day-wed.png icons/day-thu.png icons/day-fri.png icons/day-sat.png fonts/Oswald-Light.ttf"
